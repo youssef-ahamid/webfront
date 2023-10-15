cf4e7e460a27a9188c5bfaa5575ff60526380cdd
Youssef Abdelhamid <youssef@tutoruu.com>
2023-10-15 20:31:05 +0300

    dynamic content

--- components/server/Content.tsx
+++ components/server/Content.tsx
@@ -1,7 +1,7 @@
 import { FontSize, fontSizes } from "@/config/fontSizes";
 import clsx from "clsx";
-
-interface ContentProps
+import content from "@/content.json";
+export interface ContentProps
   extends React.HTMLAttributes<
     | HTMLHeadingElement
     | HTMLParagraphElement
@@ -10,13 +10,24 @@ interface ContentProps
   > {
   as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6" | "p" | "span" | "q";
   size?: FontSize;
+  contentId?: ContentKey;
 }
+
+export type ContentKey = keyof typeof content;
+
 export default function Content({
   as: Component = "p",
   size = "body/md",
   className,
+  contentId,
+  children,
+  contentEditable,
   ...props
 }: ContentProps) {
+  if (contentId) {
+    children = content[contentId];
+    // contentEditable = await isAdminView(); // TODO: check if content admin
+  }
   return <Component {...props} className={clsx(fontSizes[size], className)} />;
 }
 
