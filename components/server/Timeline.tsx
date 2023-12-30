2f75a5e21a96cd767931f1d3e6d4f57ebab3a558
Youssef Abdelhamid <youssef@tutoruu.com>
2023-12-30 21:35:05 +0200

    mobile timeline view

--- components/server/Timeline.tsx
+++ components/server/Timeline.tsx
@@ -24,15 +24,15 @@ export default function Timeline({ events }: { events: TimelineEvent[] }) {
       {events.map((event, index) => (
         <div
           className={clsx(
-            "w-full flex justify-center py-12 relative",
-            index % 2 === 0 ? "flex-row" : "flex-row-reverse"
+            "w-full flex flex-col-reverse md:justify-center pt-8 md:pt-16 pb-36 relative",
+            index % 2 === 0 ? "md:flex-row" : "md:flex-row-reverse"
           )}
           key={index}
         >
           <div
             className={clsx(
-              "w-full md:max-w-[50%] flex flex-col space-y-2 px-12",
-              index % 2 === 0 ? "items-end" : "items-start"
+              "w-full md:max-w-[50%] flex flex-col space-y-2 px-4 md:px-12 items-start",
+              index % 2 === 0 ? "md:items-end" : "md:items-start"
             )}
           >
             <Appear delay={0.1}>
@@ -51,32 +51,34 @@ export default function Timeline({ events }: { events: TimelineEvent[] }) {
               </Appear>
             </div>
           </div>
-          <div className="absolute top-0 left-1/2 -translate-x-1/2">
+          <div className="absolute top-0 left-0 md:left-1/2 -translate-x-1/2">
             <Appear delay={0.4}>
               <div className="w-4 h-4 bg-default rounded-full relative">
                 <div className="absolute w-4 h-4 bg-default rounded-full animate-ping"></div>
                 <Content
                   size="header/md"
                   contentId={event.year}
-                  className="absolute left-1/2 translate-x-8 -top-12"
+                  className="absolute left-1/2 translate-x-8 -top-16"
                 />
               </div>
             </Appear>
           </div>
           {event.image && (
-            <div className="w-full md:max-w-[50%] px-12">
+            <div className="w-full md:max-w-[50%] px-4 md:px-12">
               <Appear delay={0.4}>
                 <Image
                   src={event.image}
+                  width={500}
+                  height={500}
                   alt={event.title}
-                  className="max-w-lg"
+                  className="max-w-full my-6 md:max-w-lg"
                 />
               </Appear>
             </div>
           )}
         </div>
       ))}
-      <div className="absolute left-1/2 top-10 bottom-0 overflow-hidden pb-32 border-l-2 border-default border-spacing-16  border-dashed">
+      <div className="absolute left-6 md:left-1/2 top-10 bottom-0 overflow-hidden pb-32 border-l-2 border-default border-dashed">
         <div className="h-full w-1 -translate-x-[1.5px]"></div>
       </div>
     </PaddedContent>
