2b8b115dfccec2799858edbb055f07a76183174c
Youssef Abdelhamid <youssef@tutoruu.com>
2023-12-30 21:42:17 +0200

    add missing about images + about seo

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,8 @@
 import { Hero, Page, Section, Timeline } from "@/components";
-import { Owner, Speech } from "@/images";
+import { getSeoForPage } from "@/config/seo";
+import { MoussyEvent, Presentation, Speech, Owner } from "@/images";
+
+export const generateMetadata = getSeoForPage("/about");
 
 const events = [
   {
@@ -7,70 +10,70 @@ const events = [
     year: "timeline-1-year",
     date: "timeline-1-date",
     description: "timeline-1-subtitle",
-    image: Speech,
+    image: Owner,
   },
   {
     title: "timeline-2-title",
     year: "timeline-2-year",
     date: "timeline-2-date",
     description: "timeline-2-subtitle",
-    image: Speech,
+    image: Owner,
   },
   {
     title: "timeline-3-title",
     year: "timeline-3-year",
     date: "timeline-3-date",
     description: "timeline-3-subtitle",
-    image: Speech,
+    image: Owner,
   },
   {
     title: "timeline-4-title",
     year: "timeline-4-year",
     date: "timeline-4-date",
     description: "timeline-4-subtitle",
-    image: Speech,
+    image: Owner,
   },
   {
     title: "timeline-5-title",
     year: "timeline-5-year",
     date: "timeline-5-date",
     description: "timeline-5-subtitle",
-    image: Speech,
+    image: Owner,
   },
   {
     title: "timeline-6-title",
     year: "timeline-6-year",
     date: "timeline-6-date",
     description: "timeline-6-subtitle",
-    image: Speech,
+    image: Owner,
   },
   {
     title: "timeline-7-title",
     year: "timeline-7-year",
     date: "timeline-7-date",
     description: "timeline-7-subtitle",
-    image: Speech,
+    image: Owner,
   },
   {
     title: "timeline-8-title",
     year: "timeline-8-year",
     date: "timeline-8-date",
     description: "timeline-8-subtitle",
-    image: Speech,
+    image: Owner,
   },
   {
     title: "timeline-9-title",
     year: "timeline-9-year",
     date: "timeline-9-date",
     description: "timeline-9-subtitle",
-    image: Speech,
+    image: Owner,
   },
   {
     title: "timeline-10-title",
     year: "timeline-10-year",
     date: "timeline-10-date",
     description: "timeline-10-subtitle",
-    image: Speech,
+    image: Owner,
   },
 ];
 
@@ -80,7 +83,7 @@ export default function About() {
       <Hero
         title="hero-title"
         subtitle="hero-subtitle"
-        graphicUrl={Owner}
+        graphicUrl={Presentation}
         color="success"
         titleContent="Driven by Vision: Ayman Shahin's Legacy in Egypt's FMCG Landscape"
         subtitleContent="Established in 1991 by the visionary Ayman Shahin, the Ayman Shahin Group stands today as a testament to entrepreneurial spirit and dedication in Egypt's fast-moving consumer goods (FMCG) sector. From its inception in Port Said, Ayman Shahin's dream was to not only introduce premium global brands to the Egyptian market but also to build a legacy of excellence and innovation."
@@ -91,7 +94,7 @@ export default function About() {
         header="section-1-header"
         color="success"
         subtitle="section-1-subtitle"
-        graphicUrl={Owner}
+        graphicUrl={MoussyEvent}
         subtitleContent={`Under Ayman Shahin's leadership, the group has flourished, becoming one of Egypt's foremost names in FMCG distribution and manufacturing. Our journey, spanning over three decades, is marked by the relentless pursuit of quality, growth, and consumer satisfaction. We've expanded our reach to include a diverse array of products and services, continually adapting to meet the evolving needs of our consumers.`}
       />
       <Timeline events={events} />
