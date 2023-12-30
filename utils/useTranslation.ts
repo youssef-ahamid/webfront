42a45b69effba7a04ab780b78b2b5fdc5c55ab86
Youssef Abdelhamid <youssef@tutoruu.com>
2023-12-30 09:23:09 +0200

    useTranslation hook

---
+++ utils/useTranslation.ts
@@ -0,0 +1,9 @@
+import { useContent } from "@/contexts/Content";
+import { DictionaryKey, dictionary } from "./dictionary";
+
+export const useTranslation = () => {
+  const { lang } = useContent();
+
+  return <T extends DictionaryKey>(key: T) =>
+    dictionary.get(lang, key) as (typeof dictionary.map)[typeof lang][T];
+};
