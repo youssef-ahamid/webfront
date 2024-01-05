000bb9d5cb8afcc4ceede5696ee14b9d1b834eb1
Youssef Abdelhamid <youssef@tutoruu.com>
2024-01-06 01:28:00 +0200

    loading spinner

---
+++ app/loading.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+export default function Loading() {
+  return (
+    <div className="w-screen h-screen bg-white flex justify-center items-center fixed">
+      <div className="loader container">
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+        <div className="dot"></div>
+      </div>
+      <svg width="0" height="0" className="svg">
+        <defs>
+          <filter id="uib-jelly-ooze">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="3" result="blur" />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -7"
+              result="ooze"
+            />
+            <feBlend in="SourceGraphic" in2="ooze" />
+          </filter>
+        </defs>
+      </svg>
+    </div>
+  );
+}
