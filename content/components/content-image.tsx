f8ce1c6a299e332b5984eef0a9071c712e0312c5
Youssef Abdelhamid <youssef@tutoruu.com>
2024-01-05 15:39:58 +0200

    content image

---
+++ content/components/content-image.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import clsx from "clsx";
+import { useContent, useUser } from "@/contexts";
+import { useEffect, useMemo } from "react";
+import Image from "next/image";
+
+export type ContentImageProps<T extends "img" | typeof Image> = {
+  as?: T;
+  contentId?: string;
+  textContent?: string;
+} & React.ComponentProps<T>;
+
+import * as images from "@/images";
+
+export function ContentImage<T extends "img" | typeof Image>({
+  as: Component = "p" as any,
+  className,
+  contentId,
+  children,
+  contentEditable,
+  color,
+  textContent,
+  ...props
+}: ContentImageProps<T>) {
+  const { user } = useUser();
+  const { edit, content, openImageSelector } = useContent();
+  const type = Component === "img" || Component === Image ? "image" : "text";
+
+  const id = !contentId ? null : contentId;
+  if (id) {
+    contentEditable = !!user;
+  }
+
+  const src = useMemo(() => {
+    console.log(content);
+    if (type !== "image") return;
+    if (id && (content as any)?.[id]) {
+      if ((content as any)?.[id] in images)
+        return images[(content as any)?.[id] as keyof typeof images] as any;
+      return (content as any)?.[id];
+    }
+    if ("src" in props) {
+      if ((props.src as any) in images)
+        return images[props.src as keyof typeof images] as any;
+      return props.src;
+    }
+  }, [id, props, type, content]);
+
+  useEffect(() => {
+    if (contentEditable && id && "src" in props && !content?.[id]) {
+      edit(id, props.src as any);
+    }
+  }, [id, props]);
+
+  return (
+    <Component
+      {...(props as any)}
+      src={src}
+      onClick={() => {
+        if (!contentEditable || !id) return;
+        openImageSelector(id);
+      }}
+      suppressContentEditableWarning
+      style={{
+        outline: "none",
+        ...props.style,
+      }}
+      className={clsx(className, contentEditable && "cursor-pointer")}
+      children={undefined}
+    />
+  );
+}
