1597847662aea12f403d6a28c07939c802e82375
Youssef Abdelhamid <youssef@tutoruu.com>
2024-01-15 18:17:02 +0200

    remove last subsidiary

--- components/interactive/subsidiaries.tsx
+++ components/interactive/subsidiaries.tsx
@@ -14,7 +14,6 @@ const links = [
   "/business/manufacturing",
   "/business/manufacturing",
   "/business/manufacturing",
-  "/business/manufacturing",
 ];
 export function Subsidiaries() {
   return (
